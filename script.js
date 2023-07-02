912ca301893a90bda34fc087d0171c3105e23267
Santiago Bernardi <santiagobernardi13@gmail.com>
2023-07-01 22:47:44 -0300

    subiendo primera version

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict"
+
+document.querySelector("#menu-bar").addEventListener("click", show);
+document.querySelector("#close").addEventListener("click", show);
+
+function show() {
+    document.querySelector("nav").classList.toggle("show");
+}
+
+document.querySelector("main").addEventListener("scroll", function() {
+    console.log("scroll");
+    document.querySelector(".arrow").classList.toggle("hide");
+});
+
+
+// Detectamos cuando el usuario desplace la pantalla
+window.onscroll = function (){
+    // Obtenemos la posicion del scroll en pantall
+    var scroll = document.documentElement.scrollTop || document.body.scrollTop;
+
+    // Realizamos alguna accion cuando el scroll este entre la posicion 300 y 400
+    if(scroll > 300){
+        console.log("Pasaste la posicion 300 del scroll");
+        document.querySelector(".arrow").classList.add("hide");
+        
+    }
+    if(scroll < 300){
+        document.querySelector(".arrow").classList.remove("hide");
+        
+    }
+}
